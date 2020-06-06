3c7e6c29a3a770db9b43cb108c8fd5b23d45e46c
Ken Holston <git.kholston@gmail.com>
2020-06-05 23:48:26 -0400

    Add #edit and #destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,4 +18,15 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to action: 'index'
+  end
 end

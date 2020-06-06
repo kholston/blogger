eeea14259f527da5691f5b54346769c60ef4ad0a
Ken Holston <git.kholston@gmail.com>
2020-06-06 00:05:44 -0400

    Add #update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(article)
+  end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
